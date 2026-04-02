7ecdc887aadec0b2af71315861aaca75fd4de6ac
Ritesh Swain <152496676+Ritesh001-Git@users.noreply.github.com>
2026-04-03 02:31:38 +0530

    S3 by terraform

--- infra-app/s3.tf
+++ infra-app/s3.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "${var.env}-${var.bucket_name}"
 
   tags = {
-    NAME = "${var.env}-${var.bucket_name}"
-    ENVIRONMENT = var.env
+    Name        = "${var.env}-${var.bucket_name}"
+    Environment = var.env
   }
 }
